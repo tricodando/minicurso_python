c224ce44535d64c0ef5b6ced92d5f3659e2157d0
Tricodando <85141942+tricodando@users.noreply.github.com>
2022-03-30 08:53:44 -0300

    Add files via upload

---
+++ bemvindo.py
@@ -0,0 +1,8 @@
+"""
+    Lab (Seja bem vindo a Tricodando!)
+
+    Problema: Capturar o nome da pessoa pelo teclado
+    e dar as boas vindas no shell com uma frase personalizada.
+"""
+nome = input("Digite o seu nome e tecle enter: ")
+print("Olá %s, seja bem vindo a Tricodando!" % nome)
