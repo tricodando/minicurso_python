98e8ff5bf0b3ad7b986813fd924834318dae6ed1
Tricodando <85141942+tricodando@users.noreply.github.com>
2022-03-30 07:13:05 -0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+"""
+    Lab (I Love Python)
+
+    Problema: Imprimir na tela do shell a seguinte
+    frase "I Love Python!"
+"""
+frase = "Hello Python"
+print(frase)
+
+inteiro = 10
+decimal = 3.14
+verdadeiro = True
+falso = False
